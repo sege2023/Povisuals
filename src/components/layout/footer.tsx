3392fe5be645fdd6fe772fc856f051b36c784f29
sege2023 <shittumalik91@gmail.com>
2025-03-25 00:34:16 +0100

    added googlesapi

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,21 @@
+import { Copyright } from "lucide-react"
 const Footer = () =>{
     return(
         <>
-            
+           <footer>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+                <p> <Copyright/> 2025 POVISUALS</p>
+                <ul>
+                    <li><a href=""><Gith</a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+           </footer>
         </>
     )
 }
